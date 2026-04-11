b62807aabad2f7c9cc01fd5262468052d43f857a
niridium <niridium@tuta.io>
2026-04-11 22:50:09 +0200

    fix(archive.sh): check for existing tar archive refactor(archive.sh): update directories and run zstd with nix

--- archive.sh
+++ archive.sh
@@ -1,9 +1,9 @@
-if [[ ! -f $HOME/bash_scripts/config/archive.conf ]] then
+if [[ ! -f $HOME/my_scripts/config/archive.conf ]] then
 	echo "Configuration file not found, you can find a sample at 'config' directory"
 	exit 2
 fi
 
-source ~/bash_scripts/config/archive.conf
+source ~/my_scripts/config/archive.conf
 
 if [[ -z $1 || -n $2 ]] then 
 	echo "Incorrect number of arguments"
@@ -13,11 +13,13 @@ then
 	echo "Target doesn't exist"
 	exit 1
 else
-	INPUT=$(echo $1 | sed -e 's#/$##') #Sanitize string removing last slash
+	INPUT=$(echo $1 | sed -e 's#/$##') #Sanitize string removing last slash, correctly getting INPUT top directory
 
-	tar -cf "$INPUT.tar" "$INPUT"
-	echo "Tarball created"
+	if [[ ! -f $INPUT.tar ]] then
+		tar -cf "$INPUT.tar" "$INPUT"
+		echo "Tarball created"
+	fi
 
 	echo "Compressing..."
-	zstd $ZSTD_PARAMETERS "$INPUT.tar"
+	nix run nixpkgs#zstd -- $ZSTD_PARAMETERS "$INPUT.tar"
 fi
